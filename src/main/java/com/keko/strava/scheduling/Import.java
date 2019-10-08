8b9f03877d5417548aff58717b07d7c56bed9083
Cleriston <cleriston@gmail.com>
2019-10-08 16:31:13 -0300

    Implement import running

--- src/main/java/com/keko/strava/scheduling/Import.java
+++ src/main/java/com/keko/strava/scheduling/Import.java
@@ -1,6 +1,7 @@
 package com.keko.strava.scheduling;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -24,39 +26,33 @@ public class Import {
 	@Autowired
 	private ActivityRepository activityRepository;
 
-	//@Scheduled(fixedRate = 300000)
+	@Scheduled(fixedRate = 300000)
 	public void importActivities() {
+		List<Activity> activities = new ArrayList<Activity>();
+		do {
 		
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-		Principal principal = (Principal) authentication.getPrincipal();
-		List<Activity> lastActivity = activityRepository.findFirstByOrderByStartDateDesc();
-
-		int lastEpoch = 0;
-		if (lastActivity.isEmpty() == false) {
-			lastEpoch = (int) (lastActivity.get(0).getStartDate().getTime() / 1000);
-		}
-		System.out.println(lastEpoch);
-		final RestTemplate restTemplate = new RestTemplate();
-
-		final HttpHeaders headers = new HttpHeaders();
-		headers.setBearerAuth(getAccessToken(principal));
-		final HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		ResponseEntity<List<Activity>> rateResponse = restTemplate.exchange(
-				"https://www.strava.com/api/v3/athlete/activities?after=" + lastEpoch, HttpMethod.GET, entity,
-				new ParameterizedTypeReference<List<Activity>>() {
-				});
-		List<Activity> activities = rateResponse.getBody();
-
-		activityRepository.saveAll(activities);
+			List<Activity> lastActivity = activityRepository.findFirstByOrderByStartDateDesc();
+	
+			int lastEpoch = 0;
+			if (lastActivity.isEmpty() == false) {
+				lastEpoch = (int) (lastActivity.get(0).getStartDate().getTime() / 1000);
+			}
+			
+			final RestTemplate restTemplate = new RestTemplate();
+	
+			final HttpHeaders headers = new HttpHeaders();
+			headers.setBearerAuth("ce80698258c77df2e06d6599d953f6c7311f6a7a");
+			final HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+			ResponseEntity<List<Activity>> rateResponse = restTemplate.exchange(
+					"https://www.strava.com/api/v3/athlete/activities?after=" + lastEpoch, HttpMethod.GET, entity,
+					new ParameterizedTypeReference<List<Activity>>() {
+					});
+			activities = rateResponse.getBody();
+	
+			activityRepository.saveAll(activities);
+		}while (activities.size()==30);
+			
 	}
 
-	private String getAccessToken(final Principal principal) {
-
-		final OAuth2Authentication oauth2Auth = (OAuth2Authentication) principal;
-		final OAuth2AuthenticationDetails oauth2AuthDetails = (OAuth2AuthenticationDetails) oauth2Auth.getDetails();
-
-		return oauth2AuthDetails.getTokenValue();
-	}
-
+	
 }
